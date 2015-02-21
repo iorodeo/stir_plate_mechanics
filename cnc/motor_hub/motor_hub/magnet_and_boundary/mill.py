813c6c52fc1125a08473813419031d7fc68e9ac5
Will Dickson <will@iorodeo.com>
2015-02-20 18:50:03 -0800

    Added test alignment option to magnet hole and boundary mill operation.

--- cnc/motor_hub/motor_hub/magnet_and_boundary/mill.py
+++ cnc/motor_hub/motor_hub/magnet_and_boundary/mill.py
@@ -6,16 +6,21 @@ from py2gcode import cnc_pocket
 from py2gcode import cnc_boundary
 from params import params
 
+alignTest = False 
+
 # Cutting parameters
 safeZ = 0.5
 startZ = 0.0
 overlap = 0.4
 overlapFinish = 0.6
-maxCutDepth = 0.03
-toolDiam = 0.25
+maxCutDepth = 0.05
+if alignTest:
+    toolDiam = 0.001
+else:
+    toolDiam = 0.25
 direction = 'ccw'
 startDwell = 2.0
-feedrate = 22.0
+feedrate = 24.0
 cutThruMargin = 0.04
 
 prog = gcode_cmd.GCodeProg()
@@ -35,11 +40,16 @@ for i,posLayout in enumerate(zip(params['xPosList'],params['yPosList'])):
         for xPosRel, yPosRel in zip(magnetParams['xPosList'], magnetParams['yPosList']):
             xPos = xPosLayout + xPosRel
             yPos = yPosLayout + yPosRel
+            if alignTest:
+                depth = maxCutDepth
+            else:
+                depth = magnetParams['depth']
+
             pocketDict = { 
                     'centerX'        : xPos, 
                     'centerY'        : yPos,
                     'radius'         : 0.5*magnetParams['diameter'],
-                    'depth'          : magnetParams['depth'],
+                    'depth'          : depth,
                     'startZ'         : startZ,
                     'safeZ'          : safeZ,
                     'overlap'        : overlap,
@@ -49,23 +59,33 @@ for i,posLayout in enumerate(zip(params['xPosList'],params['yPosList'])):
                     'direction'      : direction,
                     'startDwell'     : startDwell,
                     }
-            pocket = cnc_pocket.CircPocketXY(pocketDict)
+            if alignTest:
+                pocketDict['thickness'] = toolDiam
+                pocket = cnc_pocket.CircAnnulusPocketXY(pocketDict)
+            else:
+                pocket = cnc_pocket.CircPocketXY(pocketDict)
             prog.add(pocket)
 
     if 1:
-        if i == (params['numParts'] - 1):
-            prog.add(gcode_cmd.Space())
-            prog.add(gcode_cmd.Comment('Pause'))
-            prog.add(gcode_cmd.Pause())
-            prog.add(gcode_cmd.Space())
+        #if i == (params['numParts'] - 1):
+        #    prog.add(gcode_cmd.Space())
+        #    prog.add(gcode_cmd.Comment('Pause'))
+        #    prog.add(gcode_cmd.Pause())
+        #    prog.add(gcode_cmd.Space())
+
+        boundaryParams = params['boundaryCut']
+
+        if alignTest:
+            depth = maxCutDepth
+        else:
+            depth = boundaryParams['depth'] + cutThruMargin
 
         # Part boundary
-        boundaryParams = params['boundaryCut']
         boundaryDict = { 
                 'centerX'      : xPosLayout,
                 'centerY'      : yPosLayout,
                 'radius'       : boundaryParams['radius'],
-                'depth'        : boundaryParams['depth'] + cutThruMargin,
+                'depth'        : depth, 
                 'startZ'       : startZ,
                 'safeZ'        : safeZ,
                 'toolDiam'     : toolDiam,
